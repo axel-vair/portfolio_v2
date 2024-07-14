803d30c31b4f1862b772e5b8007265f140d3fe96
Axel Vair <axel@Macbook.local>
2024-07-14 17:49:12 +0200

    add ogimages

--- app.vue
+++ app.vue
@@ -1,7 +1,16 @@
+<script setup lang="ts">
+useSeoMeta({
+  title: "Portfolio d'Axel Vair",
+  ogTitle: 'Portfolio d\'Axel Vair',
+  description: 'Axel Vair est un développeur web et mobile full stack basé sur Marseille. Il a une expérience variée et une soif d\'apprendre qui lui permet de réaliser des projets de qualité.',
+  ogDescription: 'Axel Vair est un développeur web et mobile full stack basé sur Marseille. Il a une expérience variée et une soif d\'apprendre qui lui permet de réaliser des projets de qualité.',
+  ogImage: 'https://axelvair.fr/',
+  twitterCard: 'summary_large_image',
+})
+</script>
+
 <template>
   <div>
     <NuxtLayout />
   </div>
 </template>
-<script setup lang="ts">
-</script>
