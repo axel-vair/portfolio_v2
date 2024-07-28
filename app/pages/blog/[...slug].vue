3707d2d9324408fd828b3c27c9c19e61e8f19ef5
Axel <axel@Macbook.local>
2024-07-28 20:48:55 +0200

    :lipstick: fix css typo markdown

--- app/pages/blog/[...slug].vue
+++ app/pages/blog/[...slug].vue
@@ -4,7 +4,7 @@ const slug = route.params.slug
 </script>
 
 <template>
-  <article>
+  <article class="prose dark:prose-invert prose-pre:bg-white dark:prose-pre:bg-gray-800 prose-pre:text-gray-700 dark:prose-pre:text-gray-300">
     <ContentDoc />
   </article>
 </template>
